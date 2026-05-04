b2768edcebb19db27bc34362516011de33177e5f
Slavi <Slavi@DESKTOP-FCSTPIA>
2026-05-04 20:41:09 +0300

    Challenge systems shows error when there arent enough trajectories to challenge

--- WorldKartIdentity/Controllers/TrackController.cs
+++ WorldKartIdentity/Controllers/TrackController.cs
@@ -320,20 +320,23 @@ namespace WorldKartIdentity.Controllers
                     t.Id != challengerTrajId)
                 .ToListAsync();
 
-            
+            if(challengedTrajs.Count < 1)
+            {
                 TempData["Message"] = "Няма достатъчно траектории за предизвикване. Моля, изчакайте други състезатели да качат своите траектории на тази писта.";
                 return RedirectToAction("TrajectoryDetails", new { id = challengerTrajId });
-            
-            //var rnd = new Random();
-            //var challengedTraj = challengedTrajs[rnd.Next(challengedTrajs.Count)];
+            }
+                
 
-            //var challenge = new ChallengeViewModel
-            //{
-            //    ChallengerTrajectory = TrackTrajectoryViewModel.TrajectoryToTrajectoryVM(challengerTraj),
-            //    ChallengedTrajectory = TrackTrajectoryViewModel.TrajectoryToTrajectoryVM(challengedTraj)
-            //};
+            var rnd = new Random();
+            var challengedTraj = challengedTrajs[rnd.Next(challengedTrajs.Count)];
 
-            //return View(challenge);
+            var challenge = new ChallengeViewModel
+            {
+                ChallengerTrajectory = TrackTrajectoryViewModel.TrajectoryToTrajectoryVM(challengerTraj),
+                ChallengedTrajectory = TrackTrajectoryViewModel.TrajectoryToTrajectoryVM(challengedTraj)
+            };
+
+            return View(challenge);
         }
 
         [HttpPost("/tracks/challenge-result")]
